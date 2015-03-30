755ba3e9d9b95625570833fcf67411655b0f9789
mira-p <pravitasari.m@gmail.com>
2015-03-30 19:41:48 +0700

    daftar feedback UI

--- application/views/detil_kelas.php
+++ application/views/detil_kelas.php
@@ -53,7 +53,138 @@
                                 <i>MTK, </i>
                                 <i>IPA, </i>
                                 <i>SMA</i>
+                                <br>
+                                <p>
+                                Untuk mendaftar kelas, Anda dapat membayar dengan Cash ataupun Transfer.<br>
+                                Anda memiliki waktu 24 jam dari sekarang untuk melaksanakan pembayaran.<br>
+
+                                Jika dalam batas waktu tersebut, Anda belum melakukan pembayaran, maka pesanan Anda akan dianggap dibatalkan, dan Anda harus mengulang proses pemesanan dari awal.<br>
+
+                                Anda dapat melakukan pembayaran langsung ke:<br>
+
+                                Ruangguru.com HQ<br>
+                                d/a Jalan Tebet Raya 32A Jakarta Selatan<br>
+                                (patokan: depan Wisma Tebet)<br>
+                                Telp: 021-9200-3040<br>
+                                </p>
+                                <button type="submit" class="btn btn-default main-button register">Daftar</button>
                             </div><!-- detil-kelas -->
+                            <div role="tabpanel" class="tab-pane" id="partisipan">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr class="text-center">
+                                                <td>ID</td>
+                                                <td>Nama Murid</td>
+                                                <td>Action</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Saqib</td>
+                                                <td class="status">
+                                                    <a href="#" class="cancel icon-circle" title="Unapprove"><i class="fa fa-check"></i></a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Saqib 2</td>
+                                                <td class="status">
+                                                    <a href="#" class="cancel icon-circle" title="Unapprove"><i class="fa fa-check"></i></a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div><!-- tab-partisipan -->
+                            <div role="tabpanel" class="tab-pane" id="feedback">
+                                <div class="table-responsive">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <div class="panel panel-primary">
+                                                    <div class="panel-heading">
+                                                        <i class="fa fa-list-ul"></i>
+                                                        <span>Daftar Feedback</span>
+                                                    </div>
+                                                    <div class="panel-body">
+                                                        <ul class="chat">
+                                                            <li class="left clearfix"><span class="chat-img pull-left">
+                                                                <img src="http://placehold.it/50/55C1E7/fff&amp;text=U" alt="User Avatar" class="img-circle">
+                                                            </span>
+                                                                <div class="chat-body clearfix">
+                                                                    <div class="header">
+                                                                        <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                                                                            <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                                                                    </div>
+                                                                    <p>
+                                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                                                        dolor, quis ullamcorper ligula sodales.
+                                                                    </p>
+                                                                </div>
+                                                            </li>
+                                                            <li class="right clearfix"><span class="chat-img pull-right">
+                                                                <img src="http://placehold.it/50/FA6F57/fff&amp;text=ME" alt="User Avatar" class="img-circle">
+                                                            </span>
+                                                                <div class="chat-body clearfix">
+                                                                    <div class="header">
+                                                                        <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                                                        <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                                                                    </div>
+                                                                    <p>
+                                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                                                        dolor, quis ullamcorper ligula sodales.
+                                                                    </p>
+                                                                </div>
+                                                            </li>
+                                                            <li class="left clearfix"><span class="chat-img pull-left">
+                                                                <img src="http://placehold.it/50/55C1E7/fff&amp;text=U" alt="User Avatar" class="img-circle">
+                                                            </span>
+                                                                <div class="chat-body clearfix">
+                                                                    <div class="header">
+                                                                        <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                                                                            <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+                                                                    </div>
+                                                                    <p>
+                                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                                                        dolor, quis ullamcorper ligula sodales.
+                                                                    </p>
+                                                                </div>
+                                                            </li>
+                                                            <li class="right clearfix"><span class="chat-img pull-right">
+                                                                <img src="http://placehold.it/50/FA6F57/fff&amp;text=ME" alt="User Avatar" class="img-circle">
+                                                            </span>
+                                                                <div class="chat-body clearfix">
+                                                                    <div class="header">
+                                                                        <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>15 mins ago</small>
+                                                                        <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                                                                    </div>
+                                                                    <p>
+                                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                                                        dolor, quis ullamcorper ligula sodales.
+                                                                    </p>
+                                                                </div>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="panel-footer">
+                                                        <div class="input-group">
+                                                            <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here...">
+                                                            <span class="input-group-btn">
+                                                                <button class="btn btn-warning btn-sm" id="btn-chat">
+                                                                    Send</button>
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div><!-- tab-Feedback -->
+                            
                         </div><!-- tab-content -->
 
                     </div><!-- tabpanel kelas -->
