9584b59e861575fed38e069f8dd5486468bb02bf
tsaqibb <saqib.abud@gmail.com>
2015-03-30 17:46:09 +0700

    base_url

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -12,13 +12,13 @@
 
         <!-- Bootstrap core CSS -->
         <link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-        <script type="text/javascript" src="js/jquery.fancybox.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" media="screen"/>
-        <script type="text/javascript" src="js/jquery.fancybox-media.js?v=1.0.6"></script>
+        <link rel="stylesheet" href="<?php echo base_url();?>css/font-awesome.min.css">
+        <script type="text/javascript" src="<?php echo base_url();?>js/jquery.fancybox.js"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/jquery.fancybox.css" media="screen"/>
+        <script type="text/javascript" src="<?php echo base_url();?>js/jquery.fancybox-media.js?v=1.0.6"></script>
 
         <!-- Custom styles for this template -->
-        <link href="css/style.css" rel="stylesheet">
+        <link href="<?php echo base_url();?>css/style.css" rel="stylesheet">
 
     </head>
     <body>
@@ -27,14 +27,14 @@
             <div class="container">
                 <div class="row">
                     <div class="navbar-header">
-                        <a class="navbar-brand visible-xs" href="#"><img src="images/logo-kelas-2.png" height="60"></a>
+                        <a class="navbar-brand visible-xs" href="#"><img src="<?php echo base_url();?>images/logo-kelas-2.png" height="60"></a>
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand hidden-xs" href="#"><img src="images/logo-kelas-2.png" height="60"></a>
+                        <a class="navbar-brand hidden-xs" href="#"><img src="<?php echo base_url();?>images/logo-kelas-2.png" height="60"></a>
                     </div>
                     <div id="navbar" class="navbar-collapse collapse">
                         <button data-target=".navbar-collapse" data-toggle="collapse" class="close-toggle visible-xs" type="button">
@@ -52,4 +52,3 @@
                 </div>
             </div>
         </nav>
-        <?php echo base_url();?>
