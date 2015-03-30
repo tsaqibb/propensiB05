a6fc1888c9fd2eaa6bc5d9aca65c565294d6d7bd
mira-p <pravitasari.m@gmail.com>
2015-03-30 18:40:26 +0700

    Revert "commit dari ayu"
    This reverts commit ff380b1ae2331bdc0817f93f291495b03f1412ab.

--- application/controllers/detil.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class detil extends CI_Controller {
-		$this->load->view('layout/header');
-		$this->load->view('detil_kelas');
-		$this->load->view('layout/footer');
-	}
-}
