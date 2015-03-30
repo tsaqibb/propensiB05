5ded8bd462344e46fc55a4dbe2d1f697037c84a2
vaskodagama <vaskodagama54@gmail.com>
2015-03-31 00:24:17 +0700

    Revert "Ubah retrieve materi"
    This reverts commit e52f44619ea80f2f4a42fc693bb6dca02214b007.

--- application/controllers/murid.php
+++ application/controllers/murid.php
@@ -1,15 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Murid extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
-		$this->load->helper('url');
-	}
-
 	public function index()
 	{
 		$this->load->view('layout/header');
-		$this->load->view('galeri_kelas');
+		$this->load->view('kelas_anda');
 		$this->load->view('layout/footer');
 	}
 
