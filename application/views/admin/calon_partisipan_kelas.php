168cffacc0f32d30d9daf2e84613ba2fa0e833e6
Wahyu Retnowati <sangsurya01@rocketmail.com>
2015-04-11 15:31:35 +0700

    update calon_partisipan

--- application/views/admin/calon_partisipan_kelas.php
+++ application/views/admin/calon_partisipan_kelas.php
@@ -1,3 +1,11 @@
+<script>
+function toggle(source) {
+  checkboxes = document.getElementsByTagName('input');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
+</script>
 <script type="text/javascript">
   function show_confirm()
   {
@@ -12,9 +20,9 @@
     }
   }
 </script>
-                <h2 class="block-title judul text-uppercase">Daftar Calon Murid</h2><br><hr class="line"><br>
+                <h2 class="block-title judul text-uppercase">DAFTAR CALON MURID</h2><br><hr class="line"><br>
                     <!--<div role="tabpanel" class="tab-pane active" id="partisipan">-->
-                        <input class ="checkbox1" type="checkbox">select all</input>
+                        <input class ="checkbox1" type="checkbox" onclick="toggle(this)">select all</input>
                             <a type="button" onclick="show_confirm()" value="Show confirm box" class="main-button register2">Activate all</a>
                                 <br><br>
                                     <table class="table">
@@ -22,8 +30,8 @@
                                           <td>
                                             <input class ="checkbox" type="checkbox"></input>
                                           </td>
-                                          <td class="nama">Nama Murid Pendaftar Pertama - <a href=""> 1206238715</a><br>
-                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online I</a>
+                                          <td class="nama">Nama Murid Pertama - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online </a>
                                             <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
                                           </td>
                                           <td class="status">
@@ -34,8 +42,8 @@
                                           <td>
                                             <input class ="checkbox" type="checkbox"></input>
                                           </td>
-                                          <td class="nama">Nama Murid Pendaftar Kedua - <a href=""> 1206238715</a><br>
-                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online II</a>
+                                          <td class="nama">Nama Murid Kedua - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online</a>
                                             <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
                                           </td>
                                           <td class="status">
@@ -46,8 +54,68 @@
                                           <td>
                                             <input class ="checkbox" type="checkbox"></input>
                                           </td>
-                                          <td class="nama">Nama Murid Pendaftar Ketiga - <a href=""> 1206238715</a><br>
-                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online III</a>
+                                          <td class="nama">Nama Murid Ketiga - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online</a>
+                                            <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
+                                          </td>
+                                          <td class="status">
+                                            <a href="#" onclick="show_confirm()" value="Show confirm box" class="approve icon-button"><i class="fa fa-check"></i>Activate</a>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td>
+                                            <input class ="checkbox" type="checkbox"></input>
+                                          </td>
+                                          <td class="nama">Nama Murid keempat - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online</a>
+                                            <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
+                                          </td>
+                                          <td class="status">
+                                            <a href="#" onclick="show_confirm()" value="Show confirm box" class="approve icon-button"><i class="fa fa-check"></i>Activate</a>
+                                          </td>
+                                        </tr>
+                                         <tr>
+                                          <td>
+                                            <input class ="checkbox" type="checkbox"></input>
+                                          </td>
+                                          <td class="nama">Nama Murid kelima - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online</a>
+                                            <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
+                                          </td>
+                                          <td class="status">
+                                            <a href="#" onclick="show_confirm()" value="Show confirm box" class="approve icon-button"><i class="fa fa-check"></i>Activate</a>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td>
+                                            <input class ="checkbox" type="checkbox"></input>
+                                          </td>
+                                          <td class="nama">Nama Murid Keenam - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online</a>
+                                            <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
+                                          </td>
+                                          <td class="status">
+                                            <a href="#" onclick="show_confirm()" value="Show confirm box" class="approve icon-button"><i class="fa fa-check"></i>Activate</a>
+                                          </td>
+                                        </tr>
+                                         <tr>
+                                          <td>
+                                            <input class ="checkbox" type="checkbox"></input>
+                                          </td>
+                                          <td class="nama">Nama Murid Ketujuh - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online</a>
+                                            <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
+                                          </td>
+                                          <td class="status">
+                                            <a href="#" onclick="show_confirm()" value="Show confirm box" class="approve icon-button"><i class="fa fa-check"></i>Activate</a>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td>
+                                            <input class ="checkbox" type="checkbox"></input>
+                                          </td>
+                                          <td class="nama">Nama Murid Kedelapan - <a href=""> 1206238715</a><br>
+                                            <a class ="matkul" href="">Nama Mata Kuliah Kuliah Online </a>
                                             <small class="matkul text-muted"><span class="fa fa-clock-o"></span>30 Desember 2015</small>
                                           </td>
                                           <td class="status">
